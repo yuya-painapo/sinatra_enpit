70899ec1a6cd8746898727afdf19d608a1c454f9
None <k158578@ie.u-ryukyu.ac.jp>
2015-09-23 05:27:19 +0000

    Edit files for Heroku

--- hello.rb
+++ hello.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
+require 'HyakuninIssyu'
 
-get '/' do
-  "Hello World!"
-end
+#get '/' do
+#  "Hello World!"
+#end
 
 get '/yaho-' do
   "Hello World!"
   "test!"
+end
+
+get '/' do
+    data = HyakuninIssyu.new(1)
+    data.poem.kanji
 end
 
